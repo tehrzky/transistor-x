ca3622913c2270ae1fd227dc7535359cd092d5ea
tehrzky <61300808+tehrzky@users.noreply.github.com>
2025-08-14 11:25:06 +0800

    Update Constants.kt

--- app/src/main/java/org/y20k/transistor/core/Constants.kt
+++ app/src/main/java/org/y20k/transistor/core/Constants.kt
@@ -2,5 +2,5 @@ package org.y20k.transistor.core
 
 object Constants {
     const val VANILLA_ICE_CREAM = "vanilla_ice_cream"
-    const val ALERT_DIALOG_THEME = R.style.AlertDialogTheme
+    const val ALERT_DIALOG_THEME = "AlertDialogTheme" // Use string resource name instead of R reference
 }
