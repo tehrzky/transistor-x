29ced3cdae91171d77f57640685f24610d477065
tehrzky <61300808+tehrzky@users.noreply.github.com>
2025-08-14 11:39:38 +0800

    Update SearchResultAdapter.kt

--- app/src/main/java/org/y20k/transistor/search/SearchResultAdapter.kt
+++ app/src/main/java/org/y20k/transistor/search/SearchResultAdapter.kt
@@ -1,117 +1,68 @@
-/*
- * SearchResultAdapter.kt
- * Implements the SearchResultAdapter class
- * A SearchResultAdapter is a custom adapter providing search result views for a RecyclerView
- *
- * This file is part of
- * TRANSISTOR - Radio App for Android
- *
- * Copyright (c) 2015-25 - Y20K.org
- * Licensed under the MIT-License
- * http://opensource.org/licenses/MIT
- */
+package org.y20k.transistor.dialogs
 
-
-package org.y20k.transistor.search
-
+import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.y20k.transistor.R
 import org.y20k.transistor.core.Station
+import org.y20k.transistor.search.SearchResultAdapter
 
+class AddStationDialog(
+    private val context: Context, 
+    private val stationList: List<Station>, 
+    private val listener: AddStationDialogListener
+) : SearchResultAdapter.SearchResultAdapterListener {
 
-/*
- * SearchResultAdapter class
- */
-class SearchResultAdapter(private val listener: SearchResultAdapterListener, var searchResults: List<Station>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    /* Define log tag */
-    private val TAG: String = SearchResultAdapter::class.java.simpleName
-
-
-    /* Main class variables */
-    private var selectedPosition: Int = RecyclerView.NO_POSITION
-
-
-    /* Listener Interface */
-    interface SearchResultAdapterListener {
-        fun onSearchResultTapped(result: Station)
+    interface AddStationDialogListener {
+        fun onAddStationDialog(station: Station)
     }
 
+    private val TAG = AddStationDialog::class.java.simpleName
+    private lateinit var dialog: AlertDialog
+    private lateinit var stationSearchResultList: RecyclerView
+    private lateinit var searchResultAdapter: SearchResultAdapter
+    private var station: Station = Station()
 
-    init {
-        setHasStableIds(true)
-    }
-
-
-    /* Overrides onCreateViewHolder from RecyclerView.Adapter */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.element_search_result, parent, false)
-        return SearchResultViewHolder(v)
-    }
-
-
-    /* Overrides getItemCount from RecyclerView.Adapter */
-    override fun getItemCount(): Int {
-        return searchResults.size
-    }
-
-
-    /* Overrides getItemCount from RecyclerView.Adapter */
-    override fun getItemId(position: Int): Long = position.toLong()
-
-
-    /* Overrides onBindViewHolder from RecyclerView.Adapter */
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        // get reference to ViewHolder
-        val searchResultViewHolder: SearchResultViewHolder = holder as SearchResultViewHolder
-        val searchResult: Station = searchResults[position]
-        // update text
-        searchResultViewHolder.nameView.text = "${searchResult.name} (${searchResult.codec} - ${searchResult.bitrate} kbps)"
-        searchResultViewHolder.streamView.text = searchResult.getStreamUri()
-        // mark selected if necessary
-        searchResultViewHolder.searchResultLayout.isSelected = selectedPosition == position
-        // toggle text scrolling (marquee) if necessary
-        searchResultViewHolder.nameView.isSelected = selectedPosition == position
-        searchResultViewHolder.streamView.isSelected = selectedPosition == position
-        // attach touch listener
-        searchResultViewHolder.searchResultLayout.setOnClickListener {
-            // move marked position
-            notifyItemChanged(selectedPosition)
-            selectedPosition = position
-            notifyItemChanged(selectedPosition)
-            // hand over station
-            listener.onSearchResultTapped(searchResult)
+    override fun onSearchResultTapped(result: Station) {
+        station = result
+        if (::dialog.isInitialized) {
+            activateAddButton()
         }
     }
 
+    fun show() {
+        val builder = MaterialAlertDialogBuilder(context, R.style.Theme_Transistor_AlertDialog)
+            .setTitle(R.string.dialog_add_station_title)
+            .setPositiveButton(R.string.dialog_find_station_button_add) { _, _ ->
+                listener.onAddStationDialog(station)
+            }
+            .setNegativeButton(R.string.dialog_generic_button_cancel, null)
 
-    /* Resets the selected position */
-    fun resetSelection(clearAdapter: Boolean) {
-        val currentlySelected: Int = selectedPosition
-        selectedPosition = RecyclerView.NO_POSITION
-        if (clearAdapter) {
-            searchResults = listOf()
-            notifyDataSetChanged()
-        } else {
-            notifyItemChanged(currentlySelected)
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_add_station, null)
+        stationSearchResultList = view.findViewById(R.id.station_list)
+        setupRecyclerView(context)
+
+        dialog = builder.setView(view).create()
+        dialog.show()
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+    }
+
+    private fun setupRecyclerView(context: Context) {
+        searchResultAdapter = SearchResultAdapter(this, stationList)
+        stationSearchResultList.apply {
+            adapter = searchResultAdapter
+            layoutManager = object : LinearLayoutManager(context) {
+                override fun supportsPredictiveItemAnimations() = true
+            }
+            itemAnimator = DefaultItemAnimator()
         }
     }
 
-
-
-    /*
-     * Inner class: ViewHolder for a radio station search result
-     */
-    private inner class SearchResultViewHolder (var searchResultLayout: View): RecyclerView.ViewHolder(searchResultLayout) {
-        val nameView: MaterialTextView = searchResultLayout.findViewById(R.id.station_name)
-        val streamView: MaterialTextView = searchResultLayout.findViewById(R.id.station_url)
+    private fun activateAddButton() {
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = true
     }
-    /*
-     * End of inner class
-     */
-
 }
